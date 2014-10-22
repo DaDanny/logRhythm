33b2a32323a0cd6deafcf3e01e2ed91722c0d160
DaDanny <DanFrancken@gmail.com>
2014-10-21 17:27:18 -0700

    Modulus and github repo initalized

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://logr:admin@ds047050.mongolab.com:47050/logrhythm'
+  },
+
+  seedDB: true
+};
